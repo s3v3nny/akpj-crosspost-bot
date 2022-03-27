54bebfa8bd512b6dc3872033d806194349f00339
Arseny Korotaev <s3v3nny@ya.ru>
2022-03-27 16:04:46 +0400

    2

--- src/main/java/ru/s3v3nny/akpjbot/Main.java
+++ src/main/java/ru/s3v3nny/akpjbot/Main.java
@@ -14,32 +14,33 @@ public class Main {
     // TODO: задача со звездочкой:
     //  вынести весь код в отдельный класс, сделать все методы во всех остальных классах НЕ статичными
     //  в этом методе оставить только создание объекта класса в который переместишь код и вызов метода
-    public static void main(String... args) throws IOException, InterruptedException {
+    public static void main(String... args) throws IOException, InterruptedException, TelegramApiException {
 
         PostInfo postInfo = new PostInfo();
         Updates updates = postInfo.updates.get(0);
         Attachments attachments = postInfo.updates.get(0).object.attachments.get(0);
+        TelegramBot bot = new TelegramBot();
 
         try {
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-            telegramBotsApi.registerBot(new TelegramBot());
-            // TODO: если выпала ошибка, то что произойдет?
+            telegramBotsApi.registerBot(bot);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
+        // TODO: если выпала ошибка, то что произойдет?
+
 
         do {
 
             postInfo = HttpRequestToVK.getAndParseInfo();
 
-            if((postInfo.failed != null)) {
+            if ((postInfo.failed != null)) {
                 postInfo = HttpRequestToVK.getAndParseInfo();
             }
 
 
-
             // TODO: вынести postInfo.updates.get(0) в переменную - +
-            if(updates.object.marked_as_ads == 1) continue;
+            if (updates.object.marked_as_ads == 1) continue;
             // TODO: вынести postInfo.updates.get(0).object.attachments.get(0) в переменную - +
 
             int index = 0;
@@ -61,14 +62,10 @@ public class Main {
             tgPostInfo.text = postInfo.updates.get(0).object.text;
             tgPostInfo.pictureURL = postInfo.updates.get(0).object.attachments.get(0).photo.sizes.get(index).url;
 
-            // TODO: зачем? ты же уже создал бота выше
-            TelegramBot bot = new TelegramBot();
-
-
             bot.sendPic(tgPostInfo);
             System.out.println("картин очка отправлена!");
 
-        }while(true);
+        } while (true);
 
 
     }
