7468e5f94a0e5c2357dc0f3c4042ef25ff406a19
Arseny Korotaev <s3v3nny@ya.ru>
2022-04-28 15:53:58 +0400

    null attachments suggested post crash fix

--- src/main/java/ru/s3v3nny/akpjbot/Main.java
+++ src/main/java/ru/s3v3nny/akpjbot/Main.java
@@ -81,9 +81,9 @@ public class Main {
 
             if (postInfo.updates.size() == 0) continue;
             if ("suggest".equals(postInfo.updates.get(0).object.post_type)) continue;
+            if (postInfo.updates.get(0).object == null) continue;
             if (postInfo.updates.get(0).object.attachments.size() == 0) continue;
             if (postInfo.updates.get(0).object.attachments.get(0).photo == null) continue;
-
 
 
             Updates updates = postInfo.updates.get(0);
